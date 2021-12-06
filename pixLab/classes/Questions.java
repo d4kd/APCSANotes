10750ab1fbc667fbe755f1985af9518936d6e320
Jose Carmona Santos 0692 <d4kd@fresnou.org>
2021-12-06 12:26:08 -0800

    12/6/2021
    12:26 pm

--- pixLab/classes/Questions.java
+++ pixLab/classes/Questions.java
@@ -9,24 +9,24 @@ public class Questions
 {
     // A1: Quetsions
     // 1.How many bits does it take to represent the values from 0 to 255?
-    // It takes ____ bits to represent the values from 0 to 255.
+    // It takes 8 bits to represent the values from 0 to 255.
     // 2.How many bytes does it take to represent a color in the RGB color model?
-    // It takes ____ bytes to represent a color in the RGB color model.
+    // It takes 8 bytes to represent a color in the RGB color model.
     // 3.How many pixels are in a picture that is 640 pixels wide and 480 pixels high?
-    // There are ____ pixels in a picture that is 640 pixels wide and 480 pixels high.
-    
+    // There are 307,200 pixels in a picture that is 640 pixels wide and 480 pixels high.
+
     // A2: Questions
     // 1.How can you make pink?
-    // You can make ___ by ____.
+    // You can make pink by using r=255 g=0 b=255.
     // 2.How can you make yellow?
-    // You can make ___ by ____.
+    // You can make yellow by using r=255 g=255 b=0.
     // 3.How can you make purple?
-    // You can make ___ by ____.
+    // You can make purple by using r=204 g=0 b=255.
     // 4.How can you make white? 
-    // You can make ___ by ____.
+    // You can make white by using r=255 g=255 b=255.
     // 5.How can you make dark gray?
-    // You can make ___ by ____.
-    
+    // You can make gray by using r=102 g=102 b=102.
+
     // A3: Questions
     // 1.What is the row index for the top left corner of the picture?
     // The row index for the top left corner of the picture is 0.
@@ -43,16 +43,35 @@ public class Questions
     // 7.Set the zoom to 500%. Can you see squares of color?
     // This is called pixelation. Pixelation means displaying a picture so magnified that the individual pixels look like small squares.
     // The squares of color can be better identified when zoomed in at 500%.
-    
-    // A4: Questions
-    
-    // 
-    
-    // 
-    
-    // 
-    
-    // 
-    
-    // 
+
+    // A5: Questions
+    // 1.Open Picture.java and look for the method getPixels2D.Is it there?
+    // It is not there.
+    // 2.Open SimplePicture.java and look for the method getPixels2D.Is it there?
+    // It is there.
+    // 3.Does the following code compile?
+    // DigitalPicture p = new DigitalPicture();
+    // ____, the following code __ compile.
+    // 4.Assuming that a no-argument constructor exists for SimplePicture,would the following code compile?
+    // DigitalPicture p = new SimplePicture();
+    // ____, the following code __ compile.
+    // 5.Assuming that a no-argument constructor exists for Picture,does the following code compile?
+    // DigitalPicture p = new Picture();
+    // ____, the following code __ compile.
+    // 6.Assuming that a no-argument constructor exists for Picture,does the following code compile?
+    // SimplePicture p = new Picture();
+    // ____, the following code __ compile.
+    // 7.Assuming that a no-argument constructor exists for SimplePicture,does the following code compile?
+    // Picture p = new SimplePicture(); 
+    //  ____, the following code __ compile.
+
+    // A7: Questions
+    // 1.How many times would the body of this nested for loop execute?
+    // for (int row = 7; row < 17; row++)
+    // for (int col = 6; col < 15; col++)
+    // 10 - row, 9 - col = 90 counts
+    // 2.How many times would the body of this nested for loop execute?
+    // for (int row = 5; row <= 11; row++)
+    // for (int col = 3; col <= 18; col++)
+    // 6 - row, 15 - col = 112 counts
 }
